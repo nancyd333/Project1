4bacd6f340b7bcabebfc262de3a9ddd32e97347e
nancyd333 <rgba0001@gmail.com>
2022-12-04 16:00:47 -0500

    refactoring code and starting astroid and fuel again once it hits supernova

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ const playerFuelSpan = document.createElement('span')
 const playerAstroidImg = document.createElement('img')
 const playerAstroidSpan = document.createElement('span')
 const ctx = canvas.getContext('2d')
+let imageHeight = 30
+let imageWidth = 30
 let astroidTracker = 0
 let fuelTracker = 0
 let leftMainHeight = leftMain.clientHeight//offsetHeight as opposed to clientHeight
@@ -155,18 +157,69 @@ function createSuperNova(){
 
 //==ASTROIDS AND FUEL==//
 
+// base object used to create astroid and fuel objects
+const baseImageArray= {
+    name: '',
+    src: '',
+    width: '',
+    height: '',
+    xcord: '',
+    ycord: '',
+    alt: '',
+    id: '',
+    type: '',
+    intervalId: '',
+    imgLocId: ''
+}
+
+//fuel object storing information about fuel
+// function createBaseFuelObject(){
+    const fuelImg = Object.create(baseImageArray);
+    fuelImg.name = 'fuelImage' 
+    fuelImg.src = fuelPic
+    fuelImg.width = imageWidth
+    fuelImg.height = imageHeight
+    fuelImg.xcord = 0
+    fuelImg.ycord = 0
+    fuelImg.alt = fuelAlt
+    // fuelImg.id = ''
+    fuelImg.type = 'fuel'
+    fuelImg.intervalId = ''
+    fuelImg.imgLocId = ''
+
+// }
+
+//astroid object storing information about astroid
+// function createBaseAstroidObject(){
+    const astroidImg = Object.create(baseImageArray);
+    astroidImg.name = 'astroidImage' 
+    astroidImg.src = astroidPic
+    astroidImg.width = imageWidth
+    astroidImg.height = imageHeight
+    astroidImg.xcord = 0
+    astroidImg.ycord = 0
+    astroidImg.alt = astroidAlt
+    // astroidImg.id = ''
+    astroidImg.type = 'astroid'
+    astroidImg.intervalId = ''
+    astroidImg.imgLocId = ''
+
+// }
+
+
+
 //image array to track images
 //this is only called on set up
 //numstart allows for loading of fuel and then astroid
 //num is the number of items you want to create in the array
-function createImageArray(num, name, src, width, height, xcord, ycord,alt, type){
-    let pixelCounter = 0
+function createImageArray(name, src, width, height, xcord, ycord,alt, type){
+    // let pixelCounter = 0
     let numstart = 0
     if(!imgArray.length){
         numstart = 0
     } else numstart=imgArray.length
 
-    for(let i=numstart; i<num+numstart; i++){
+    for(let i=numstart; i<numstart+1; i++){
         imgArray.push(new Image())
         imgArray[i].name = name  + i
         imgArray[i].src = src
@@ -176,24 +229,26 @@ function createImageArray(num, name, src, width, height, xcord, ycord,alt, type)
         imgArray[i].ycord = ycord
         imgArray[i].alt = alt
         imgArray[i].display = "none"
-        imgArray[i].id = name + i
+        // imgArray[i].id = name + i
         imgArray[i].type = type
         imgArray[i].intervalId = ''
+        imgArray[i].imgLocId = ''
+        imgArray[i].num = i
 
         //check value
-        console.log("createImageArray", imgArray[i].name, imgArray[i].src, imgArray[i].width,imgArray[i].height,imgArray[i].xcord,imgArray[i].ycord,imgArray[i].alt,imgArray[i].display,imgArray[i].id ,imgArray[i].intervalId,"numstart",numstart)
+        console.log("createImageArray", imgArray[i].name, imgArray[i].src, imgArray[i].width,imgArray[i].height,imgArray[i].xcord,imgArray[i].ycord,imgArray[i].alt,imgArray[i].display,imgArray[i].id ,imgArray[i].intervalId,"numstart",numstart,"locId",imgArray[i].imgLocId, 'i', i)
 
     }
 
 }
 
-//get the positions for the current screen and use to determine the location of the images
+//get the positions for the current browser screen and use to determine the location of the images
 //need all images added to array before this can be calcualted
 function populateImgLocationsArray(){
     let counter = 0
     imgLoc = Math.floor(leftMainWidth * .90 / imgArray.length)   
         //check value
-        console.log("imgLoc",imgLoc)
+        console.log("imgLoc",imgLoc, imgArray.length)
 
     for(let i=0; i < imgArray.length; i++ ){
         counter += imgLoc
@@ -204,20 +259,11 @@ function populateImgLocationsArray(){
     
 }
 
-//changes image locations so they don't populate in the same location every time
-function swapImageLocations(loc1, loc2){
-    val1 = imgArray[loc1].xcord
-    val2 = imgArray[loc2].xcord
-    
-    imgArray[loc1].xcord = val2
-    imgArray[loc2].xcord = val1
-    
-}
-
 //set image location to the images array
 function setImageLocations(){
     for(let i = 0; i < imgArray.length; i++){
         imgArray[i].xcord = imgLocations[i]
+        imgArray[i].imgLocId = imgLocations[i]
     }
         //check value
         // console.log('check xcord assignment')    
@@ -236,6 +282,7 @@ function setImageIntervals(){
     }
 }
 
+//create the Player 1 section for Fuel with image
 function createPlayerFuelMessage(){
     playerFuelImg.src = fuelPic
     playerFuelImg.alt = fuelAlt
@@ -247,6 +294,7 @@ function createPlayerFuelMessage(){
     playerFuelSpan.innerText = `Fuel: ${fuelTracker}`
 }
 
+//create the Player 1 section for Astroid with image
 function createPlayerAstroidMessage(){
     playerAstroidImg.src = astroidPic
     playerAstroidImg.alt = astroidAlt
@@ -258,14 +306,57 @@ function createPlayerAstroidMessage(){
     playerAstroidSpan.innerText = `Astroid: ${astroidTracker}`
 }
 
+//creates a astroid or fuel image, chosen at random
+//num is the number of total images you want to create
+//assumes screen position will be assigned at this step, such as on game load
 function createRandomImage(num){
     for(let i=0; i < num; i++){
         if(getRandomInteger(0,1)==1){
-            createImageArray(1,'fuelImage',fuelPic,30,30,0,0,fuelAlt,'fuel')
+            createImageArray(fuelImg.name,fuelImg.src,fuelImg.width,fuelImg.height,fuelImg.xcord,fuelImg.ycord,fuelImg.alt,fuelImg.type)
         } else {
-            createImageArray(1,'astroidImage',astroidPic,30,30,0,0,astroidAlt,'astroid')
+            createImageArray(astroidImg.name,astroidImg.src,astroidImg.width,astroidImg.height,astroidImg.width,astroidImg.height,astroidImg.alt,astroidImg.type)
         }
     }
+}
+
+//once astroid/fuel hits supernova a random fuel/astroid needs to be added to that array location and fall from the ycord 0 but the same xcord as the one that hit the supernova, also keeping the same interval id
+
+function recycleRandomImage(array){
+    ctx.clearRect(array.xcord, array.ycord, array.width, array.height)
+    //check value
+    console.log("recycleImage before", array)
+    for(let i=0; i < 1; i++){
+        if(getRandomInteger(0,1)==1){
+            array.name = astroidImg.name + array.num
+            array.src = astroidImg.src
+            array.width = astroidImg.width
+            array.height = astroidImg.height
+            array.ycord = 0
+            array.alt = astroidImg.alt
+            array.type = astroidImg.type
+            clearInterval(array.intervalId)
+            interval = getRandomInteger(200,1000)
+            array.intervalId = setInterval(()=>{moveImage(array,array.type)},interval)
+            ctx.drawImage(array,array.xcord,array.ycord, array.width, array.height)
+            //check value
+            console.log("recycleImage after",array)
+        } else {
+            array.name = fuelImg.name + array.num
+            array.src = fuelImg.src
+            array.width = fuelImg.width
+            array.height = fuelImg.height
+            array.ycord = 0
+            array.alt = fuelImg.alt
+            array.type = fuelImg.type
+            clearInterval(array.intervalId)
+            interval = getRandomInteger(200,1000)
+            array.intervalId = setInterval(()=>{moveImage(array,array.type)},interval)
+            ctx.drawImage(array,array.xcord,array.ycord, array.width, array.height)
+            //check value
+            console.log("recycleImage after",array)
+        }
+    }
+
 }
 
 
@@ -273,6 +364,9 @@ function createRandomImage(num){
 
 // loads the game
 function loadGame(){
+    //create objects for fuel and astroid
+    // createBaseFuelObject()
+    // createBaseAstroidObject()
 
     //populate Player 1 messages
     createPlayerFuelMessage()
@@ -292,12 +386,7 @@ function loadGame(){
   
     //== FUEL AND ASTROIDS ==//
 
-    // create fuel images 
-    //createImageArray(10,'fuelImage',fuelPic,30,30,0,0,fuelAlt,'fuel')
-    
-    // create astroid images
-   // createImageArray(6,'astroidImage',astroidPic,30,30,0,0,astroidAlt,'astroid')
-    
+    // create fuel and astroid images
     createRandomImage(10)
 
     //populates the image locations so they know where display on screen
@@ -305,14 +394,6 @@ function loadGame(){
 
     //set image location on the img array
     setImageLocations()
-
-    //rundamenary swapping of the images
-    rand1 = Math.floor(Math.random() * imgLocations.length)
-    rand2 = Math.floor(Math.random() * imgLocations.length)
-        //check value
-        console.log("random values", rand1, rand2)
-    swapImageLocations(rand1,rand2)
-    swapImageLocations(rand1,rand2)
 
     setImageIntervals()
 
@@ -434,18 +515,21 @@ function detectShipWinCollision(){
         
 }
 
-
 //collision between the supernova and the fuel/astroid
 function detectSuperNovaCollision(array){
-    if(supernovaY <= array.ycord+100){
-        clearInterval(array.intervalId)
-        array.intervalId = ''
-        clearImage(array)
+    if(supernovaY <= array.ycord+60){
+        // let recycleImgId = array.imgLocId
+        // clearInterval(array.intervalId)
+        // array.intervalId = ''
+        // clearImage(array)
+        recycleRandomImage(array)
+
     }
+
 }
 
 
-//move astroid/fuel & detect collision between (spaceship and astroid/fuel & supernova and astroid/fuel)
+//ADD WHAT HERE
 function moveImage(name, type){
     ctx.clearRect(name.xcord, name.ycord, name.width, name.height)
     name.ycord += 5
