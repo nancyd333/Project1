cf06e61cb3073b7eed2f3f100cddc3d614b0c6e1
nancyd333 <rgba0001@gmail.com>
2022-12-06 22:27:21 -0500

    continuing to refactor code to make it generic

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const canvas = document.querySelector('canvas')
 const body = document.querySelector('html')
 const leftMain = document.getElementById('left-main')
 const resetButton = document.getElementById('resetButton')
-const spaceshipImage = document.createElement('img')
+const playerImgElement = document.createElement('img')
 const userMessage = document.getElementById('user-message')
 const userMessage2 = document.getElementById('user-message2')
 const playerOneDiv = document.getElementById('player-one')
@@ -18,24 +18,14 @@ const middleTopDiv = document.getElementById('middleTopDiv')
 const ctx = canvas.getContext('2d')
 let leftMainHeight = leftMain.clientHeight//offsetHeight as opposed to clientHeight
 let leftMainWidth = leftMain.clientWidth//offsetWidth as opposed to clientWidth
-// let gradientTrackNum = 0
-// let gradientIncNo = 20
-// let gradientEndNo = leftMainWidth -200
-// let gradientChangeValue = 0
-let imageHeight = 30
-let imageWidth = 30
-let spaceshipHeight = 100
-let spaceshipWidth = 60
-let spaceshipLeftRightSpeed = 50
-let spaceshipHitMoveUpNo = 50 
-let spaceshipHitMoveDownNo = 10
+// let imageHeight = 30
+// let imageWidth = 30
 let gameStatus = 0 // gameStatus 1 is in-play and 0 is not in-play
 // let bodyHeight = body.clientHeight
 // let bodyWidth = body.clientWidth
-let spacePic = '24752-5-spaceship.png'
-let spaceAlt = 'spaceship'
-let decHitItemPic = 'astroid1.jpg'
-let incHitItemPic = 'sun.jpeg'
+
+// let decHitItemPic = 
+// let incHitItemPic = 'sun.jpeg'
 let imgArray = []
 let imgLocations = []
 let intervalList = []
@@ -95,8 +85,47 @@ class HitItem{
 
 }
 
-const incHitItem = new HitItem('fuelImage', incHitItemPic, imageWidth, imageHeight, 0,0, 'fuel','','')
-const decHitItem = new HitItem('astroidImage', decHitItemPic, imageWidth, imageHeight, 0,0, 'astroid','','')
+const incHitItem = new HitItem('fuelImage', 'sun.jpeg', 30, 30, 0,0, 'fuel','','')
+const decHitItem = new HitItem('astroidImage', 'astroid1.jpg', 30, 30, 0,0, 'astroid','','')
+
+// class to create players
+class Player{
+    constructor(name,description,src){
+        this.name = name;
+        this.description = description;
+        this.src = src;
+        this.width = 60;
+        this.height = 100;
+        this.xcord = leftMainWidth/2;
+        this.ycord = leftMainHeight-175;
+        this.alt = description.toLowerCase();
+        this.displayName = name.charAt(0).toUpperCase() + name.slice(1);
+        this.leftRightSpeed = 50;
+        this.HitIncNo = 50;
+        this.HitDecNo = 10;
+    }
+}
+
+const player1 = new Player('spaceship', 'spaceship', '24752-5-spaceship.png')
+
+// let spaceshipHeight = 100
+// let spaceshipWidth = 60
+// let spaceshipLeftRightSpeed = 50
+// let spaceshipHitMoveUpNo = 50 
+// let spaceshipHitMoveDownNo = 10
+// let spaceAlt = 'spaceship'
+// let spacePic = '24752-5-spaceship.png'
+
+
+function createPlayerImage(name, player)  {
+    name.src= player.src
+    name.width = player.width
+    name.height = player.height
+    name.xcord = player.xcord
+    name.ycord = player.ycord
+    name.alt = player.alt
+}
+
 
 //set canvas to the height / width of the div that contains it
 //canvas size is sets once and keeps it at that size
@@ -153,22 +182,22 @@ function reset(type){
     // removeEventListener('keypress',navigateSpaceship,false)
     gameStatus = 0
     // ctx.clearRect(spaceshipImage.xcord, spaceshipImage.ycord, spaceshipImage.width, spaceshipImage.height)
-    clearImage(spaceshipImage)
+    clearImage(playerImgElement)
 
-    if(type=='supernovaEvent'){
+    if(type=='eventWin'){
         for(let i = 0; i < imgArray.length; i++){
             clearImage(imgArray[i])
             clearInterval(imgArray[i].intervalId)
         }
-        clearImage(spaceshipImage)
+        clearImage(playerImgElement)
         userMessage2.innerText = ''
-        ctx.fillRect(event1.xcord,0,event1.width,event1.height)
+        ctx.fillRect(event1.xcord,0,event1.width,event1.height) //this makes the event cover the screen
     } else if (type=='resetGame'){
         for(let i = 0; i < imgArray.length; i++){
             clearImage(imgArray[i])
             clearInterval(imgArray[i].intervalId)
             //checkvalue
-            console.log('resetGame',imgArray)
+            //console.log('resetGame',imgArray)
         }
         ctx.clearRect(0,0, event1.width, event1.height)
         clearInterval(event1.intervalId)
@@ -185,7 +214,7 @@ function reset(type){
         userMessage.innerText = ''
         userMessage2.innerText = ''
         loadGame()
-    } else if (type =='winGame'){
+    } else if (type =='playerWin'){
         for(let i = 0; i < imgArray.length; i++){
             clearImage(imgArray[i])
             clearInterval(imgArray[i].intervalId)
@@ -193,7 +222,7 @@ function reset(type){
         ctx.clearRect(0, 0, event1.width, event1.height)
         clearInterval(event1.intervalId)
         event1.resetEvent()
-        clearImage(spaceshipImage)
+        clearImage(playerImgElement)
         userMessage2.innerText = ''
     }
 }
@@ -203,30 +232,20 @@ function onResetClick(){
     reset('resetGame')
     
     //check value
-    console.log('reset clicked')
+    //console.log('reset clicked')
 
 }
 
-//==SPACESHIP==> 
 
-//create spaceship image
- function createSpaceshipImage(name, src,width,height,xcord,ycord,alt)  {
-    name.src= src
-    name.width = width
-    name.height = height
-    name.xcord = xcord
-    name.ycord = ycord
-    name.alt = alt
-}
 
 //load spaceship image
-function loadSpaceshipImage(name){
+function loadPlayerImage(name){
     //generate image on page
     name.onload = function(){
-        drawImage(name) 
-        // ctx.drawImage(name, name.xcord,name.ycord,name.width,name.height)
+        // drawImage(name) 
+        ctx.drawImage(name, name.xcord,name.ycord,name.width,name.height)
         //check value
-        // console.log(name,"drawImgValues","x",name.xcord,"y",name.ycord,"width",name.width,"height",name.height)
+        console.log(name,"drawImgValues","x",name.xcord,"y",name.ycord,"width",name.width,"height",name.height)
     }
 }
 
@@ -305,7 +324,7 @@ function createImageArray(name, src, width, height, xcord, ycord,alt, type){
         imgArray[i].num = i
 
         //check value
-        console.log("createImageArray", imgArray[i].name, imgArray[i].src, imgArray[i].width,imgArray[i].height,imgArray[i].xcord,imgArray[i].ycord,imgArray[i].alt,imgArray[i].display,imgArray[i].id ,imgArray[i].intervalId,"numstart",numstart,"locId",imgArray[i].imgLocId, 'i', i)
+        //console.log("createImageArray", imgArray[i].name, imgArray[i].src, imgArray[i].width,imgArray[i].height,imgArray[i].xcord,imgArray[i].ycord,imgArray[i].alt,imgArray[i].display,imgArray[i].id ,imgArray[i].intervalId,"numstart",numstart,"locId",imgArray[i].imgLocId, 'i', i)
 
     }
 
@@ -317,13 +336,13 @@ function populateImgLocationsArray(){
     let counter = 0
     imgLoc = Math.floor(leftMainWidth * .90 / imgArray.length)   
         //check value
-        console.log("imgLoc",imgLoc, imgArray.length)
+        //console.log("imgLoc",imgLoc, imgArray.length)
 
     for(let i=0; i < imgArray.length; i++ ){
         counter += imgLoc
         imgLocations[i] = counter
             //check value
-            console.log("setcoords",counter, imgLocations)
+            // console.log("setcoords",counter, imgLocations)
     }
     
 }
@@ -378,7 +397,7 @@ function createPlayerMenuDecHitMessage(){
 //creates a astroid or fuel image, chosen at random
 //num is the number of total images you want to create
 //assumes screen position will be assigned at this step, such as on game load
-function createRandomImage(num, hitItem1, hitItem2){
+function createRandomHitImage(num, hitItem1, hitItem2){
     for(let i=0; i < num; i++){
         if(getRandomInteger(0,1)==1){
             createImageArray(hitItem1.name,hitItem1.src,hitItem1.width,hitItem1.height,hitItem1.xcord,hitItem1.ycord,hitItem1.alt,hitItem1.type)
@@ -389,7 +408,7 @@ function createRandomImage(num, hitItem1, hitItem2){
 }
 
 //once astroid/fuel hits supernova a random fuel/astroid needs to be added to that array location and fall from the ycord 0 but the same xcord as the one that hit the supernova
-function recycleRandomImage(array){
+function recycleRandomHitImage(array){
     clearImage(array)
     //check value
     // console.log("recycleImage before", array)
@@ -450,19 +469,19 @@ function loadGame(){
     //==SPACESHIP==>
 
     //create spaceship image
-    createSpaceshipImage(spaceshipImage, spacePic,spaceshipWidth,spaceshipHeight,leftMainWidth/2,leftMainHeight-175, spaceAlt)
+    //createSpaceshipImage(spaceshipImage, spacePic,spaceshipWidth,spaceshipHeight,leftMainWidth/2,leftMainHeight-175, spaceAlt)
     
         //check value
-        console.log("checkspaceshipImageCreateValues", "src", spaceshipImage.src, "img width",spaceshipImage.width, "img height", spaceshipImage.height, "img x", spaceshipImage.xcord, "img y", spaceshipImage.ycord)
+      //  console.log("checkspaceshipImageCreateValues", "src", spaceshipImage.src, "img width",spaceshipImage.width, "img height", spaceshipImage.height, "img x", spaceshipImage.xcord, "img y", spaceshipImage.ycord)
 
     //loads spaceship image
-    loadSpaceshipImage(spaceshipImage)
-
+    createPlayerImage(playerImgElement,player1)
+    loadPlayerImage(playerImgElement)
   
     //== FUEL AND ASTROIDS ==//
 
     // create fuel and astroid images
-    createRandomImage(10,incHitItem, decHitItem)
+    createRandomHitImage(10,incHitItem, decHitItem)
 
     //populates the image locations so they know where display on screen
     populateImgLocationsArray()
@@ -489,22 +508,22 @@ loadGame()
 
 //keys to move the spaceship, clear image and redraw image when moved
 //event listener for keys only work if the game status is set to in-play
-function navigateSpaceship(e){
+function navigatePlayer(e){
     
     if (e.key == 'a' && gameStatus == 1){
-        ctx.clearRect(spaceshipImage.xcord, spaceshipImage.ycord, spaceshipImage.width, spaceshipImage.height)
-        spaceshipImage.xcord -= spaceshipLeftRightSpeed
+        ctx.clearRect(playerImgElement.xcord, playerImgElement.ycord, playerImgElement.width, playerImgElement.height)
+        playerImgElement.xcord -= player1.leftRightSpeed
             // check value
             // console.log('move ss left',"xcord",spaceshipImage.xcord, "ycord",spaceshipImage.ycord,"x", spaceshipImage.x, "y", spaceshipImage.y)
-        drawImage(spaceshipImage)
+        drawImage(playerImgElement)
         //ctx.drawImage(spaceshipImage,spaceshipImage.xcord,spaceshipImage.ycord, spaceshipImage.width, spaceshipImage.height)
     }   
     if (e.key == 's' && gameStatus == 1){
-        ctx.clearRect(spaceshipImage.xcord, spaceshipImage.ycord, spaceshipImage.width, spaceshipImage.height)
-        spaceshipImage.xcord += spaceshipLeftRightSpeed
+        ctx.clearRect(playerImgElement.xcord, playerImgElement.ycord, playerImgElement.width, playerImgElement.height)
+        playerImgElement.xcord += player1.leftRightSpeed
             //check value
             // console.log('move ss right',"xcord",spaceshipImage.xcord,"ycord", spaceshipImage.ycord)
-        drawImage(spaceshipImage)
+        drawImage(playerImgElement)
         // ctx.drawImage(spaceshipImage,spaceshipImage.xcord,spaceshipImage.ycord,spaceshipImage.width, spaceshipImage.height)
     }
 
@@ -515,34 +534,34 @@ function navigateSpaceship(e){
 }
 
 // event listener to control the spaceship 
-document.addEventListener('keydown', navigateSpaceship,false)
+document.addEventListener('keydown', navigatePlayer,false)
 
 // moves spaceship based on rules for fuel / astroid collision
-function moveSpaceship(type){
+function movePlayer(type){
     // adding event listener here to TEST location and effect, in this location you can't move the spaceship until it's hit by an astroid or fuel
     // document.addEventListener('keydown', navigateSpaceship,false)
 
-    rememberSpaceShipLocX = spaceshipImage.xcord
-    rememberSpaceShipLocY = spaceshipImage.ycord
+    rememberPlayerLocX = playerImgElement.xcord
+    rememberPlayerLocY = playerImgElement.ycord
     
-    ctx.clearRect(rememberSpaceShipLocX, rememberSpaceShipLocY, spaceshipImage.width, spaceshipImage.height)
+    ctx.clearRect(rememberPlayerLocX, rememberPlayerLocY, playerImgElement.width, playerImgElement.height)
         //check value
-        console.log('clear spaceship',"xcord",spaceshipImage.xcord, "remX", rememberSpaceShipLocX, "ycord", spaceshipImage.ycord, "remY", rememberSpaceShipLocY)    
+        //console.log('clear spaceship',"xcord",player1.xcord, "remX", rememberPlayerLocX, "ycord", player1.ycord, "remY", rememberPlayerLocY)    
     if(type == "fuel"){
-        spaceshipImage.ycord -= spaceshipHitMoveUpNo
-        ctx.drawImage(spaceshipImage,rememberSpaceShipLocX,rememberSpaceShipLocY-spaceshipHitMoveUpNo,spaceshipImage.width, spaceshipImage.height)
+        playerImgElement.ycord -= player1.HitIncNo
+        ctx.drawImage(playerImgElement,rememberPlayerLocX,rememberPlayerLocY-player1.HitIncNo,playerImgElement.width, playerImgElement.height)
         incHitItem.counter += 1
         createPlayerMenuIncHitMessage()
             //check value  
-            console.log("moveSpaceshipfuel","x", spaceshipImage.xcord,"y",spaceshipImage.ycord)
+            //console.log("moveSpaceshipfuel","x", player1.xcord,"y",player1.ycord)
     }
     if (type == "astroid"){
-        spaceshipImage.ycord += spaceshipHitMoveDownNo
-        ctx.drawImage(spaceshipImage,rememberSpaceShipLocX,rememberSpaceShipLocY+spaceshipHitMoveDownNo,spaceshipImage.width, spaceshipImage.height)
+        playerImgElement.ycord += player1.HitDecNo
+        ctx.drawImage(playerImgElement,rememberPlayerLocX,rememberPlayerLocY+player1.HitDecNo,playerImgElement.width, playerImgElement.height)
         decHitItem.counter +=1
         createPlayerMenuDecHitMessage()
             //check value
-            console.log("moveSpaceshipastroid","x", spaceshipImage.xcord,"y",spaceshipImage.ycord)
+            //console.log("moveSpaceshipastroid","x", player1.xcord,"y",player1.ycord)
     }
 
 }
@@ -550,21 +569,21 @@ function moveSpaceship(type){
 //== COLLISIONS ==//
 
 // collision between the spaceship and the fuel/astroid
-function detectShipCollision(name,type){
+function detectPlayerCollision(name,type){
     //y: test1: on the same plane so you just need to check one from fuel/astroid and space ship
     //x: test2: one corner from fuel/astroid should be between both corners from spaceship
     //x: test3: other corner from fuel/astroid shold be between both corners from spaceship
     //x: test4: outer corner of spaceship to inner corner of fuel/astroid (scenario where fuel/astroid is directly above spaceship)
 
-    let test1 = (spaceshipImage.ycord <= name.ycord+imageWidth)
-    let test2 = (spaceshipImage.xcord >=name.xcord &&spaceshipImage.xcord <= name.xcord+imageWidth)
-    let test3 = (spaceshipImage.xcord+spaceshipWidth >=name.xcord &&spaceshipImage.xcord+spaceshipWidth <= name.xcord+imageWidth)
-    let test4 = (spaceshipImage.xcord < name.xcord && spaceshipImage.xcord+spaceshipWidth > name.xcord+imageWidth)
+    let test1 = (playerImgElement.ycord <= name.ycord+name.width)
+    let test2 = (playerImgElement.xcord >=name.xcord &&playerImgElement.xcord <= name.xcord+name.width)
+    let test3 = (playerImgElement.xcord+playerImgElement.width >=name.xcord &&playerImgElement.xcord+playerImgElement.width <= name.xcord+name.width)
+    let test4 = (playerImgElement.xcord < name.xcord && playerImgElement.xcord+playerImgElement.width > name.xcord+name.width)
     if ( (test1 && test2) || (test1 && test3) || (test1 && test4)){
             //check value
             // console.log(name,type,"hit spaceship")
-        recycleRandomImage(name)
-        moveSpaceship(type)
+        recycleRandomHitImage(name)
+        movePlayer(type)
         
     }
 }
@@ -572,13 +591,13 @@ function detectShipCollision(name,type){
 
 //spaceship reaches top of screen
 //win condition detected
-function detectShipWinCollision(){
-    let test1 = spaceshipImage.ycord
+function detectWinCollision(){
+    let test1 = playerImgElement.ycord
     if(test1 <= 0){
         //change user message to win
         userMessage.innerText = `You were able to escape! ${'\n'}Congratulations!`;
         //remove astroid/fuel images
-        reset('winGame')
+        reset('playerWin')
         
     }
         
@@ -587,8 +606,8 @@ function detectShipWinCollision(){
 //collision between the supernova and the fuel/astroid
 //new fuel/astroid generated
 function detectEventCollision(array){
-    if(event1.ycord <= array.ycord+spaceshipHeight){
-        recycleRandomImage(array)
+    if(event1.ycord <= array.ycord+playerImgElement.height){
+        recycleRandomHitImage(array)
     }
 
 }
@@ -604,9 +623,9 @@ function moveImage(name, type){
         //console.log("moveImage", name,"xcord",name.xcord, "ycord",name.ycord,"x", name.x, "y", name.y)
     drawImage(name)
     // ctx.drawImage(name,name.xcord,name.ycord, name.width, name.height)
-    detectShipCollision(name,type)
+    detectPlayerCollision(name,type)
     detectEventCollision(name)
-    detectShipWinCollision()
+    detectWinCollision()
 
 }
 
@@ -618,13 +637,13 @@ function moveImage(name, type){
 //detects lose condition, supernova hits spaceship
 function moveEvent(){
     startEvent()
-    if(spaceshipImage.ycord+spaceshipHeight >= event1.ycord){
+    if(playerImgElement.ycord+playerImgElement.height >= event1.ycord){
         //check condition
-        console.log("supernova hit spaceship")
+        //console.log("supernova hit spaceship")
         //cover the screen with the supernova, clear all other images
         userMessage.style.color = 'magenta'
         userMessage.innerText = `You were unable to escape the supernova ${'\n'} Game over`
-        reset('supernovaEvent')
+        reset('eventWin')
         //ctx.fillRect(event1.xcord,0,event1.width,event1.height); //ycord of 0 makes image go to top of screen
         //clearInterval(event1.interval)
                 
